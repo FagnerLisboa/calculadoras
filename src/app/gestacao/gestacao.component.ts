611873a0a3957707f35b61b12a8fe82f37eb5499
Fagner Lisboa <fnlisboa2016@gmail.com>
2022-07-23 17:25:22 -0300

    calendário

--- src/app/gestacao/gestacao.component.ts
+++ src/app/gestacao/gestacao.component.ts
@@ -6,18 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gestacao.component.scss']
 })
 export class GestacaoComponent implements OnInit {
-  private day!: string
-  private month!: string
-  private year!: string
-  private result!: string
+  
+  dataMenstruacao!: string;
+  birthDate: string = "Nenhuma data informada";
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  }
+
+  ruleMonth() {
+    let monthToValidate = this.dataMenstruacao.substring(5,7);
+
+    if (monthToValidate == "01" || monthToValidate == "02" || monthToValidate == "03") {
+
+      let day;
+      day = this.dataMenstruacao.substring(8,10);
+      day = parseInt(day) + 7;
+
+      let month;
+      month = this.dataMenstruacao.substring(5,7)
+      month = parseInt(month) + 9;
+
+      let year;
+      year = this.dataMenstruacao.substring(0,4);
+      year = parseInt(year);
+      
+      this.birthDate = day + "/" + month + "/" + year;
+      
+    }
   }
 
   calcGestacao(){
-  }
-  
+    this.ruleMonth();
+  } 
 }
 
